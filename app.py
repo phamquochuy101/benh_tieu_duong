9d0acd8a2823a18564697a6639311058a0247af4
phamquochuy101 <phmquochuy@gmail.com>
2023-11-23 01:51:53 +0700

    f

---
+++ app.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 13 09:52:03 2022
+
+@author: ANURAG
+"""
+
+import numpy as np
+import pickle
+import streamlit as st
+
+# loading the saved model
+
+loaded_model = pickle.load(open("D:/HK!_2023-2024/newtech/diabetes/trained_model.sav", "rb"))
+
+# Creating a function for Prediction
+
+def diabetes_prediction(input_data):
+    
+    
+
+    #changing the input data to a numpy array
+
+    input_data_as_numpy_array = np.asarray(input_data)
+
+    #reshape the array as we are predicting for one instance
+
+    input_data_reshaped=input_data_as_numpy_array.reshape(1,-1)
+
+    # Standarize nahi kar rahe hai
+
+    prediction = loaded_model.predict(input_data_reshaped)
+
+    print(prediction)
+
+    if (prediction[0])==0:
+        return "Người này không bị tiểu đường"
+    else:
+        return "Người này bị tiểu đường"
+        
+        
+        
+def main():
+    
+    
+    #Giving a title 
+    
+    st.title("Ứng dụng dự đoán bệnh tiểu đường")
+    
+    # Getting the input data from the user
+    
+        
+    Pregnancies = st.text_input("Số lần mang thai")
+    Glucose = st.text_input("Mức đường huyết")
+    BloodPressure = st.text_input("Huyết áp")
+    SkinThickness = st.text_input("Độ dày của da")
+    Insulin = st.text_input("Mức độ Insulin")
+    BMI = st.text_input("Chỉ số BMI")
+    DiabetesPedigreeFunction = st.text_input("Chỉ số mức dộ di truyền")
+    Age = st.text_input("Tuổi")
+    
+    
+    #Code for prediction
+    
+    diagnosis = ''
+    
+    
+    #Creating a button for prediction
+    
+    if st.button("Kết qủa kiểm tra bệnh tiểu đường"):
+        diagnosis = diabetes_prediction([Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age])
+        
+    st.success(diagnosis)
+    
+    
+if __name__ == '__main__':
+    main()
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ predicitve system.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import pickle
+
+# loading the saved model
+
+loaded_model = pickle.load(open("D:/HK!_2023-2024/newtech/diabetes/trained_model.sav", "rb"))
+
+input_data = (5,166,72,19,175,25.8,0.587,51)
+
+#changing the input data to a numpy array
+
+input_data_as_numpy_array = np.asarray(input_data)
+
+#reshape the array as we are predicting for one instance
+
+input_data_reshaped=input_data_as_numpy_array.reshape(1,-1)
+
+# Standarize nahi kar rahe hai
+
+prediction = loaded_model.predict(input_data_reshaped)
+
+print(prediction)
+
+if (prediction[0])==0:
+    print("Người này không bị tiểu đường")
+else:
+    print("Người này bị tiểu đường")
